f8c2244048da535ba0630aa6ebae5c6d76a0b75f
Jennifer Wang <jenniferwang@google.com>
2013-06-11 17:59:30 -0700

    Better style via pylint

--- CustomCardFields.py
+++ CustomCardFields.py
@@ -6,38 +6,48 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class CustomCardFields(object):
+    """Object represents custom fields in a timeline card. These fields are stored in 
+    the sourceItemId field of a timeline card via JSON serialization. This class also
+    takes care of rendering timeline card HTML for a given set up custom field parameters. """
 
-  TEMPLATE = jinja_environment.get_template('templates/card.html')
+    TEMPLATE = jinja_environment.get_template('templates/card.html')
 
-  def __init__(self, dictionary={}):
-    self.fields = dictionary;
+    def __init__(self, dictionary={}):
+        self.fields = dictionary
 
-  @classmethod
-  def getFieldsFromJson(cls, Json):
-    fields = json.loads(Json);
-    return cls(fields);
-    
-  @classmethod
-  def getFieldsFromItem(cls, item):
-    key = 'sourceItemId'
-    if key in item:
-      return cls.getFieldsFromJson(item['sourceItemId'])
-    else:
-      return cls()
+    @classmethod
+    def get_fields_from_json(cls, json_input):
+        """Loads JSON in json_input and constructs a CustomCardFields object."""
+        fields = json.loads(json_input)
+        return cls(fields)
 
-  def getJson(self):
-    return json.dumps(self.fields)
+    @classmethod
+    def get_fields_from_item(cls, item):
+        """Creates a CustomCardFields object assuming that input JSON is in item["sourceItemId"]"""
+        key = 'sourceItemId'
+        if key in item:
+            return cls.get_fields_from_json(item[key])
+        else:
+            return cls()
 
-  def renderHtml(self):
-    return CustomCardFields.TEMPLATE.render(self.fields)
+    def get_json(self):
+        """Returns json for object"""
+        return json.dumps(self.fields)
 
-  def get(self, key):
-    return self.fields[key]
+    def render_html(self):
+        """Returns html rendering of timeline card with self.fields data"""
+        return CustomCardFields.TEMPLATE.render(self.fields)
 
-  def set(self, key, val):
-    self.fields[key] = val
+    def get(self, key):
+        """Returns element in dictionary, self.fields"""
+        return self.fields[key]
 
-  def updateItem(self, item):
-    item['sourceItemId'] = self.getJson()
-    item['html'] = self.renderHtml()
-    return item;
+    def set(self, key, val):
+        """Sets element in dictionary, self.fields"""
+        self.fields[key] = val
+
+    def update_item(self, item):
+        """Updates a timeline card item's sourceItemId and html with self.fields data"""
+        item['sourceItemId'] = self.get_json()
+        item['html'] = self.render_html()
+        return item
--- main_handler.py
+++ main_handler.py
@@ -24,190 +24,190 @@ import util
 
 from google.appengine.api import memcache
 from apiclient.http import HttpError
-from CustomCardFields import CustomCardFields 
+from CustomCardFields import CustomCardFields
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
 class _BatchCallback(object):
-  """Class used to track batch request responses."""
+    """Class used to track batch request responses."""
 
-  def __init__(self):
-    """Initialize a new _BatchCallbaclk object."""
-    self.success = 0
-    self.failure = 0
+    def __init__(self):
+        """Initialize a new _BatchCallbaclk object."""
+        self.success = 0
+        self.failure = 0
 
-  def callback(self, request_id, response, exception):
-    """Method called on each HTTP Response from a batch request.
+    def callback(self, request_id, response, exception):
+        """Method called on each HTTP Response from a batch request.
 
-    For more information, see
-      https://developers.google.com/api-client-library/python/guide/batch
-    """
-    if exception is None:
-      self.success += 1
-    else:
-      self.failure += 1
-      logging.error(
-          'Failed to insert item for user %s: %s', request_id, exception)
+        For more information, see
+          https://developers.google.com/api-client-library/python/guide/batch
+        """
+        if exception is None:
+            self.success += 1
+        else:
+            self.failure += 1
+            logging.error(
+                'Failed to insert item for user %s: %s', request_id, exception)
 
 
 class MainHandler(webapp2.RequestHandler):
-  """Request Handler for the main endpoint."""
+    """Request Handler for the main endpoint."""
 
-  def _render_template(self, message=None):
-    """Render the main page template."""
-    template_values = {'userId': self.userid}
-    if message:
-      template_values['message'] = message
-    # self.mirror_service is initialized in util.auth_required.
+    def _render_template(self, message=None):
+        """Render the main page template."""
+        template_values = {'userId': self.userid}
+        if message:
+            template_values['message'] = message
+        # self.mirror_service is initialized in util.auth_required.
 
-    timeline = self.mirror_service.timeline().list().execute()
-    timeline_items = timeline.get('items', [])
+        timeline = self.mirror_service.timeline().list().execute()
+        timeline_items = timeline.get('items', [])
 
-    # sort timeline items
-    timeline_items = [(dict_["created"], dict_) for dict_ in timeline_items]
-    timeline_items.sort()
-    timeline_items = [dict_ for (key, dict_) in timeline_items]
+        # sort timeline items
+        timeline_items = [(dict_["created"], dict_) for dict_ in timeline_items]
+        timeline_items.sort()
+        timeline_items = [dict_ for (key, dict_) in timeline_items]
 
-    # unpack custom fields for easier templating
-    for i in range(len(timeline_items)):
-      item = timeline_items[i]
-      fields = CustomCardFields.getFieldsFromItem(item)
-      timeline_items[i] = dict(fields.fields.items() + item.items())
+        # unpack custom fields for easier templating
+        for i in range(len(timeline_items)):
+            item = timeline_items[i]
+            fields = CustomCardFields.get_fields_from_item(item)
+            timeline_items[i] = dict(fields.fields.items() + item.items())
 
-    template_values['timelineItems'] = timeline_items
-    template_values['subscriptions'] = self.mirror_service.subscriptions().list().execute().get('items', [])
-    template = jinja_environment.get_template('templates/index.html')
-    self.response.out.write(template.render(template_values))
+        template_values['timelineItems'] = timeline_items
+        template_values['subscriptions'] = self.mirror_service.subscriptions().list().execute().get('items', [])
+        template = jinja_environment.get_template('templates/index.html')
+        self.response.out.write(template.render(template_values))
 
-  @util.auth_required
-  def get(self):
-    """Render the main page."""
-    # Get the flash message and delete it.
-    message = memcache.get(key=self.userid)
-    memcache.delete(key=self.userid)
-    self._render_template(message)
+    @util.auth_required
+    def get(self):
+        """Render the main page."""
+        # Get the flash message and delete it.
+        message = memcache.get(key=self.userid)
+        memcache.delete(key=self.userid)
+        self._render_template(message)
 
-  @util.auth_required
-  def post(self):
-    """Execute the request and render the template."""
-    operation = self.request.get('operation')
-    # Dict of operations to easily map keys to methods.
-    operations = {
-        'newCounter': self._new_counter,
-        'deleteCounter': self._delete_counter,
-        'resetCounter': self._reset_counter,
-        'updateCounter': self._update_counter,
-    }
-    if operation in operations:
-      message = operations[operation]()
-    else:
-      message = "I don't know how to " + operation
-    # Store the flash message for 5 seconds.
-    memcache.set(key=self.userid, value=message, time=5)
-    self.redirect('/')
+    @util.auth_required
+    def post(self):
+        """Execute the request and render the template."""
+        operation = self.request.get('operation')
+        # Dict of operations to easily map keys to methods.
+        operations = {
+            'newCounter': self._new_counter,
+            'deleteCounter': self._delete_counter,
+            'resetCounter': self._reset_counter,
+            'updateCounter': self._update_counter,
+        }
+        if operation in operations:
+            message = operations[operation]()
+        else:
+            message = "I don't know how to " + operation
+        # Store the flash message for 5 seconds.
+        memcache.set(key=self.userid, value=message, time=5)
+        self.redirect('/')
 
-  def _delete_counter(self):
-    """Deletes the user-inputted timeline card."""
-    self.mirror_service.timeline().delete(id=self.request.get('itemId')).execute()
-    return 'Counter Deleted'
+    def _delete_counter(self):
+        """Deletes the user-inputted timeline card."""
+        self.mirror_service.timeline().delete(id=self.request.get('itemId')).execute()
+        return 'Counter Deleted'
 
-  @staticmethod
-  def _get_num(num):
-    """ Parses num into int. Returns 0 if num is invalid """
-    try:
-      return int(num)
-    except ValueError:
-      return 0
+    @staticmethod
+    def _get_num(num):
+        """ Parses num into int. Returns 0 if num is invalid """
+        try:
+            return int(num)
+        except ValueError:
+            return 0
 
-  def _update_counter(self):
-    """Updates the counter to user input for given timeline card."""
-    item = self.mirror_service.timeline().get(id=self.request.get('itemId')).execute()
-   
-    fields = CustomCardFields({'name': self.request.get('name'), 
-      'num': self._get_num(self.request.get('num'))})
-    fields.updateItem(item)
+    def _update_counter(self):
+        """Updates the counter to user input for given timeline card."""
+        item = self.mirror_service.timeline().get(id=self.request.get('itemId')).execute()
 
-    if 'notification' in item:
-      item.pop('notification')
-    self.mirror_service.timeline().update(id=self.request.get('itemId'), body=item).execute()
-    return 'Counter Updated'
-    
-  def _reset_counter(self):
-    """Resets the counter for given timeline card."""
-    item = self.mirror_service.timeline().get(id=self.request.get('itemId')).execute()
-    fields = CustomCardFields.getFieldsFromItem(item)
-    fields.set('num',  0)
-    fields.updateItem(item)
+        fields = CustomCardFields({'name': self.request.get('name'),
+          'num': self._get_num(self.request.get('num'))})
+        fields.updateItem(item)
 
-    if 'notification' in item:
-      item.pop('notification')
-    self.mirror_service.timeline().update(id=self.request.get('itemId'), body=item).execute()
-    return 'Counter Reset'
+        if 'notification' in item:
+            item.pop('notification')
+        self.mirror_service.timeline().update(id=self.request.get('itemId'), body=item).execute()
+        return 'Counter Updated'
 
-  def _new_counter(self):
-    """Insert a timeline item."""
-    logging.info('Inserting timeline item')
-    # Note that icons will not show up when making counters on a locally hosted web interface.
-    body = {
-      'notification': {'level': 'DEFAULT'},
-      'menuItems': [
-	{
-        'action': 'CUSTOM',
-	'id': 'increment',
-	'values': [{
-	    'displayName':"Increment",
-	    'iconUrl': util.get_full_url(self, '/static/images/up.png')}]
-	},
-	{
-        'action': 'CUSTOM',
-	'id': 'decrement',
-	'values': [{
-	    'displayName':"Decrement",
-	    'iconUrl': util.get_full_url(self, '/static/images/down.png')}]
-	},
-	{
-        'action': 'CUSTOM',
-	'id': 'reset',
-	'values': [{
-	    'displayName':"Reset",
-	    'iconUrl': util.get_full_url(self, '/static/images/reset.png')}]
-	},
-	{'action': 'SHARE'},
-	{'action': 'TOGGLE_PINNED'},
-	{'action': 'DELETE'}
-      ]
-    }
+    def _reset_counter(self):
+        """Resets the counter for given timeline card."""
+        item = self.mirror_service.timeline().get(id=self.request.get('itemId')).execute()
+        fields = CustomCardFields.get_fields_from_item(item)
+        fields.set('num',  0)
+        fields.updateItem(item)
 
-    fields = CustomCardFields({'name': self.request.get('name'), 
-      'num': self._get_num(self.request.get('num'))})
-    body = fields.updateItem(body)
+        if 'notification' in item:
+            item.pop('notification')
+        self.mirror_service.timeline().update(id=self.request.get('itemId'), body=item).execute()
+        return 'Counter Reset'
 
-    # self.mirror_service is initialized in util.auth_required.
-    self.mirror_service.timeline().insert(body=body).execute()
+    def _new_counter(self):
+        """Insert a timeline item."""
+        logging.info('Inserting timeline item')
+        # Note that icons will not show up when making counters on a locally hosted web interface.
+        body = {
+          'notification': {'level': 'DEFAULT'},
+          'menuItems': [
+            {
+            'action': 'CUSTOM',
+            'id': 'increment',
+            'values': [{
+                'displayName':"Increment",
+                'iconUrl': util.get_full_url(self, '/static/images/up.png')}]
+            },
+            {
+            'action': 'CUSTOM',
+            'id': 'decrement',
+            'values': [{
+                'displayName':"Decrement",
+                'iconUrl': util.get_full_url(self, '/static/images/down.png')}]
+            },
+            {
+            'action': 'CUSTOM',
+            'id': 'reset',
+            'values': [{
+                'displayName':"Reset",
+                'iconUrl': util.get_full_url(self, '/static/images/reset.png')}]
+            },
+            {'action': 'SHARE'},
+            {'action': 'TOGGLE_PINNED'},
+            {'action': 'DELETE'}
+          ]
+        }
 
-    # Subscribe to glassware
-    subscriptions = self.mirror_service.subscriptions().list().execute()
-    need_subscribe = True
-    for subscription in subscriptions.get('items', []):
-      if subscription.get('collection') == 'timeline':
-	need_subscribe = False
+        fields = CustomCardFields({'name': self.request.get('name'),
+          'num': self._get_num(self.request.get('num'))})
+        body = fields.updateItem(body)
 
-    if need_subscribe:
-      logging.info('Subscribing to Timeline')
-      # self.userid is initialized in util.auth_required.
-      body = {'collection': 'timeline',
-	'userToken': self.userid,
-	'callbackUrl': util.get_full_url(self, '/notify')
-      }
-      # self.mirror_service is initialized in util.auth_required.
-      try:
-	self.mirror_service.subscriptions().insert(body=body).execute()
-      except HttpError:
-	return 'A new counter was created, but notifications were not enabled. HTTPS connection required.'
+        # self.mirror_service is initialized in util.auth_required.
+        self.mirror_service.timeline().insert(body=body).execute()
 
-    return  'A new counter has been created.'
+        # Subscribe to glassware
+        subscriptions = self.mirror_service.subscriptions().list().execute()
+        need_subscribe = True
+        for subscription in subscriptions.get('items', []):
+            if subscription.get('collection') == 'timeline':
+                need_subscribe = False
+
+        if need_subscribe:
+            logging.info('Subscribing to Timeline')
+            # self.userid is initialized in util.auth_required.
+            body = {'collection': 'timeline',
+              'userToken': self.userid,
+              'callbackUrl': util.get_full_url(self, '/notify')
+            }
+            # self.mirror_service is initialized in util.auth_required.
+            try:
+                self.mirror_service.subscriptions().insert(body=body).execute()
+            except HttpError:
+                return 'A new counter was created, but notifications were not enabled. HTTPS connection required.'
+
+        return  'A new counter has been created.'
 
 MAIN_ROUTES = [
     ('/', MainHandler)
